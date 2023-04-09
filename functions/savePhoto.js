65f6b2eb5dd701009e3a22a95da7dc8c6bc722ee
eurorondon03 <eurorondon03@gmail.com>
2023-04-09 11:25:34 -0400

    iniciando con dynamodb

--- functions/savePhoto.js
+++ functions/savePhoto.js
@@ -2,24 +2,39 @@ const AWS = require("aws-sdk");
 const parser = require("lambda-multipart-parser")
 const s3 = new AWS.S3();
 const rekongnition = new AWS.Rekognition();
+const dynamodb = new AWS.DynamoDB.DocumentClient();
+const { v4: uuidv4} = require('uuid')
 
 async function saveFile(file){
-  console.log({file})
+  
   const BucketName = process.env.BUCKET_NAME;
-  console.log({BucketName})
+ 
 
   const savedFile = await s3.putObject({
     Bucket: BucketName,
     Key: file.filename,
     Body: file.content,
+    
   }).promise();
 
-  const labels = await  rekongnition.detectLabels({
+  const{ Labels} = await  rekongnition.detectLabels({
     Image : {
       Bytes: file.content,
     }
   }).promise();
-  return {savedFile, labels};
+
+  const primary_key = uuidv4();
+  const labels = Labels.map(label => label.Name);
+  await dynamodb.put({ 
+    TableName: process.env.PHOTOS_TABLE,
+    Item:{
+      primary_key,
+      name: file.filename,
+      labels,
+    }
+  }).promise();
+
+  return {primary_key, savedFile : `https://${BucketName}.s3.amazonaws.com/${file.filename}`, labels};
 }
 
 module.exports.savePhoto = async (event) => {
