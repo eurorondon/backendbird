de4ee8e89f772793378d5606e636c6074986d9f9
eurorondon03 <eurorondon03@gmail.com>
2023-05-17 12:23:33 -0400

    fix cors

--- functions/savePhoto.js
+++ functions/savePhoto.js
@@ -51,6 +51,10 @@ module.exports.savePhoto = async (event) => {
 
   return {
     statusCode: 200,
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Credentials": true,
+    },
     body: JSON.stringify({
       results,
     }),
